f9324cc2b1347215fd51966a8fae58b8351f69ce
odhiambb <odhiambb@gmail.com>
2022-02-22 08:43:59 +0300

    fixtures added

--- fibonacci/tests/test_fib.py
+++ fibonacci/tests/test_fib.py
@@ -4,11 +4,12 @@ import pytest
 # from fibonacci.my_decorators import my_parametrized
 from fibonacci.naive import fibonacci_naive
 from fibonacci.cached import fibonacci_cached, fibonacci_lru_cached
+from fixtures import time_tracker
 
 
 @pytest.mark.parametrize("fib_func", [fibonacci_naive, fibonacci_cached, fibonacci_lru_cached])
 @pytest.mark.parametrize("n, expected", [(0, 0), (1, 1), (2, 1), (20, 6765)])
 # @my_parametrized(identifiers="n,expected", values=[(0,0),(1,1),(2,1),(20,6765)])
-def test_fibonacci(fib_func: Callable[[int], int], n: int, expected: int) -> None:
+def test_fibonacci(time_tracker,fib_func: Callable[[int], int], n: int, expected: int) -> None:
     res = fib_func(n)
     assert res == expected
