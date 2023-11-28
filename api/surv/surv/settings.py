c1b948f80eb5715c7b5f2dface7e28c4127e04ed
Benard Odhiambo <43642320+henriod@users.noreply.github.com>
2023-11-28 17:06:31 +0300

    remove security flows

--- api/surv/surv/settings.py
+++ api/surv/surv/settings.py
@@ -126,8 +126,8 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = 587
-EMAIL_HOST_USER = "test12django@gmail.com"
-EMAIL_HOST_PASSWORD = "odhiambo93"
+EMAIL_HOST_USER = ""
+EMAIL_HOST_PASSWORD = ""
 EMAIL_USE_TLS = True
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
 DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
